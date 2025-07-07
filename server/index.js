aaabe930911ed4268a4001a249643341170865a5
YASSINE AIT MENSOUR <aitmensouryassine@outlook.com>
2025-07-07 16:58:48 +0100

    Use of cookie-parser middleware

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import connectDB from "./lib/db.js";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
 import profileRouter from "./routes/profile.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/user", profileRouter);
